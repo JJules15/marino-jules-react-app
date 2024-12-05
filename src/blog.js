8c9a5c6b60a664d8f86bc662797b4a265770ae3f
JJules15 <mjules@bu.edu>
2024-12-04 22:45:09 -0500

    Updated Header

--- src/blog.js
+++ src/blog.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Blog.css";
 import {useParams} from "react-router-dom";
 import blogPosts from "./blogPosts.json";
+import {Link} from "react-router-dom";
 
 const Blog = () => {
     const {id} = useParams();
